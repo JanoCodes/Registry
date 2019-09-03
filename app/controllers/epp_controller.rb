b76f3b111f3a3170899139de0f94a2e62d63b305
Artur Beljajev <artur.beljajev@internet.ee>
2019-09-04 00:08:38 +0300

    Remove `mod_epp` docs

--- app/controllers/epp_controller.rb
+++ app/controllers/epp_controller.rb
@@ -362,7 +362,9 @@ class EppController < ApplicationController
   end
 
   def epp_session_id
-    cookies[:session] # Passed by mod_epp https://github.com/mod-epp/mod-epp#requestscript-interface
+    # Passed by EPP proxy
+    # https://github.com/internetee/epp_proxy#translation-of-epp-calls
+    cookies[:session]
   end
 
   def ensure_session_id_passed
