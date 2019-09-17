fb92308c70a355710f9b81c58acd9f908e41083d
Artur Beljajev <artur.beljajev@internet.ee>
2019-09-17 18:03:49 +0300

    Improve readability

--- app/models/epp_session.rb
+++ app/models/epp_session.rb
@@ -3,11 +3,11 @@ class EppSession < ActiveRecord::Base
 
   validates :session_id, uniqueness: true, presence: true
 
-  class_attribute :limit_per_registrar
-  self.limit_per_registrar = ENV['epp_session_limit_per_registrar'].to_i
+  class_attribute :sessions_per_registrar
+  self.sessions_per_registrar = ENV['epp_sessions_per_registrar'].to_i
 
   def self.limit_reached?(registrar)
     count = where(user_id: registrar.api_users.ids).count
-    count >= limit_per_registrar
+    count >= sessions_per_registrar
   end
 end
